96915379c9f23e836c6889b5c5ec82d745d566b9
Taylor Gotfrid <gotfrid7@cs.washington.edu>
2020-12-09 18:09:32 -0800

    updated pdf to png code

--- tools/pdf-to-png.py
+++ tools/pdf-to-png.py
@@ -10,7 +10,7 @@ outputDir = 'output/'
 # Change line 11
 homeDir = '/Users/tgotfrid/Documents/CSE590G/pdf-to-png/'
 # Change line 12 and 13
-conference = 'ASSETS'
+conference = 'CHI'
 year = '2019'
 
 def convert():
@@ -21,12 +21,12 @@ def convert():
         else:
             pdfTitle = file.replace('.pdf', '')
             pages = convert_from_path(file)
-            outputPDFDir = os.path.join(outputDir, pdfTitle)
-            pathlib.Path(outputPDFDir).mkdir(parents=True, exist_ok=True)
+            # outputPDFDir = os.path.join(outputDir, pdfTitle)
+            # pathlib.Path(outputPDFDir).mkdir(parents=True, exist_ok=True)
     
             counter = 1
             for page in pages:
-                myfile = os.path.join(outputPDFDir, conference + '_' + year + '_' + pdfTitle + '_page_' + str(counter)+'.png')
+                myfile = os.path.join(outputDir, conference + year + '_' + pdfTitle + '_page_' + str(counter)+'.png')
                 counter = counter + 1
                 page.save(myfile, "PNG")
                 print(myfile)
