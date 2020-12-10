ba453c46cc7c91d10c4a5be9405a8042f7247a79
Taylor Gotfrid <gotfrid7@cs.washington.edu>
2020-12-09 17:16:54 -0800

    fixed bug in pdf-to-png

--- tools/pdf-to-png.py
+++ tools/pdf-to-png.py
@@ -1,14 +1,15 @@
 import sys
 import pathlib
-print(sys.executable)
+# print(sys.executable)
 from pdf2image import convert_from_path
 import os
 
-# print("Hello")
+print("Hello")
 
 outputDir = 'output/'
+# Change line 11
 homeDir = '/Users/tgotfrid/Documents/CSE590G/pdf-to-png/'
-
+# Change line 12 and 13
 conference = 'ASSETS'
 year = '2019'
 
@@ -20,12 +21,14 @@ def convert():
         else:
             pdfTitle = file.replace('.pdf', '')
             pages = convert_from_path(file)
+            outputPDFDir = os.path.join(outputDir, pdfTitle)
+            pathlib.Path(outputPDFDir).mkdir(parents=True, exist_ok=True)
     
             counter = 1
             for page in pages:
-                myfile = os.path.join(outputDir, conference + '_' + year + '_' + pdfTitle + '-page-' + str(counter)+'.png')
+                myfile = os.path.join(outputPDFDir, conference + '_' + year + '_' + pdfTitle + '_page_' + str(counter)+'.png')
                 counter = counter + 1
                 page.save(myfile, "PNG")
-                # print(myfile)
+                print(myfile)
 
 convert()
